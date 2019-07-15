7e9fd53d6341b9b410c5a14a57314570a171362e
seburath <seburath@gmail.com>
2019-07-15 13:37:07 -0500

    message_reader

--- read_channel.py
+++ read_channel.py
@@ -1,7 +1,33 @@
-from telethon.tl.functions.contacts import ResolveUsernameRequest
+import asyncio
+import time
+import datetime
+from telethon import TelegramClient
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+from telethon import utils
 
-client = TelegramClient(session_file, api_id=X, api_hash='X')
-client.connect()
-response = client.invoke(ResolveUsernameRequest("username"))
-print(response.channel_id)
-print(response.access_hash)
+async def telelgram_response():
+    await client.send_message(nickname, 'Recived on Telegram, executing order...')
+
+async def telelgram_read():
+    async for msg in client.iter_messages(nickname, limit=10):
+        print(msg.id, msg.to_id.user_id, msg.date, msg.message)
+
+if __name__ ==  '__main__':
+    #from
+    phone = '+593xxxxxxxxx'
+    api_id = 666666
+    api_hash = '4bc685e77f57d5bxxxxxxxxxx'
+    #to
+    nickname = 'jannasp'
+
+    client = TelegramClient('session1', api_id, api_hash).start(phone)
+    client.start()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(telelgram_response())
+
+    last_id = read('.last_id.txt')
+    while True:
+        time.sleep(1)
+        loop.run_until_complete(telelgram_read())
+        print(datetime.datetime.now())
