936452552a39b03b3b024a1a1f814a9284f93444
seburath <seburath@gmail.com>
2020-11-15 22:43:47 -0500

    Add msgs redirection

--- read_channel.py
+++ read_channel.py
@@ -5,29 +5,46 @@ from telethon import TelegramClient
 from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from telethon import utils
 
-async def telelgram_response():
-    await client.send_message(nickname, 'Recived on Telegram, executing order...')
 
-async def telelgram_read():
-    async for msg in client.iter_messages(nickname, limit=10):
-        print(msg.id, msg.to_id.user_id, msg.date, msg.message)
+msgs = []
+translator_msgs = []
+
+async def send(user, msg):
+    await client.send_message(user, msg)
+
+async def receive(user):
+    async for msg in client.iter_messages(user, limit=2):
+        msgs.append(msg)
+    return msgs
+
 
 if __name__ ==  '__main__':
-    #from
-    phone = '+593xxxxxxxxx'
-    api_id = 666666
-    api_hash = '4bc685e77f57d5bxxxxxxxxxx'
-    #to
-    nickname = 'jannasp'
+    phone = '+593988306013'
+    api_id = 655586
+    api_hash = '4bc685e77f57d5b2bc92f7eef3af5b8f'
+
+    myuser = 'seburath'
+    translator = 'YTranslateBot'
 
     client = TelegramClient('session1', api_id, api_hash).start(phone)
     client.start()
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(telelgram_response())
 
-    last_id = read('.last_id.txt')
     while True:
-        time.sleep(1)
-        loop.run_until_complete(telelgram_read())
-        print(datetime.datetime.now())
+        time.sleep(2)
+        
+        msgs = []
+        msgs = loop.run_until_complete(receive(myuser))
+        is_recent = True
+
+        if msgs[-1].message == 't' and is_recent:
+
+            loop.run_until_complete(send(translator, msgs[-2].message))
+            print('translating:' + msgs[-1].message)
+            time.sleep(1)
+            translator_msgs = []
+            translator_msgs = loop.run_until_complete(receive(translator))
+
+            if translator_msgs[-2].message:
+                loop.run_until_complete(send(myuser, translator_msgs[-2].message))
